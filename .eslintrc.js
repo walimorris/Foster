1c647e2311699cdbd55df55e645d93de99c8d7e5
Wali Morris <walimmorris@gmail.com>
2021-07-04 19:53:53 -0700

    add eslint, travis ci, and updates to views

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true,
+        "jest": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+        "no console": "off",
+    },
+};
--- libs/handlers.js
+++ libs/handlers.js
@@ -1,4 +1,10 @@
 exports.home = (request, response) => response.render('home');
 exports.about = (request, response) => response.render('about');
 exports.notFound = (request, response) => response.render('404');
+
+// Express handles this error by way of the four arguments. We need the next argument,
+// though ESLint will complain about it. Therefore we will disable this line for ESlint.
+/* eslint-disable no-unused-vars */
 exports.serverError = (error, request, response, next) => response.render('500');
+
+/* eslint-enable no-unused-vars */
