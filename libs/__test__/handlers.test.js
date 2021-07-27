a0f1961ec5d01abfce4f3452755b9db83a14889e
Wali Morris <walimmorris@gmail.com>
2021-07-27 08:37:25 -0700

    add unit test for pages

--- libs/__test__/handlers.test.js
+++ libs/__test__/handlers.test.js
@@ -6,3 +6,24 @@ test('home page renders', () => {
     handlers.home(request, response);
     expect(response.render.mock.calls[0][0]).toBe('home');
 });
+
+test('about page renders', () => {
+    const request = {};
+    const response = { render: jest.fn() };
+    handlers.about(request, response);
+    expect(response.render.mock.calls[0][0]).toBe('about');
+});
+
+test('locations page renders', () => {
+    const request = {};
+    const response = { render: jest.fn() };
+    handlers.locations(request, response);
+    expect(response.render.mock.calls[0][0]).toBe('locations');
+});
+
+test('404 page renders', () => {
+    const request = {};
+    const response = { render: jest.fn() };
+    handlers.notFound(request, response);
+    expect(response.render.mock.calls[0][0]).toBe('404');
+});
